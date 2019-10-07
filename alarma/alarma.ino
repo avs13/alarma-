0acc9b77f0cfe52f9ab3763fc605a66339d68c1b
oscar <oscardavidbustillo@gmail.com>
2019-10-06 23:00:50 -0500

    se creo el sesor y se empeso a configurar los envio y recepcion de datos

--- alarma/alarma.ino
+++ alarma/alarma.ino
@@ -1,21 +1,31 @@
 
-
 #include <Wire.h>
 #include <Adafruit_GFX.h>   
 #include <Adafruit_SSD1306.h> 
+#include <EEPROM.h>
+
 #define ANCHO 128
 #define ALTO 64    
-#define OLED_RESET 4  
+#define OLED_RESET 4
+#define CE_PIN 9
+#define CSN_PIN 10
+
+byte direccion[5] ={'c','a','n','a','l'};
+
+RF24 radio(CE_PIN, CSN_PIN);
+
 Adafruit_SSD1306 oled(ANCHO, ALTO, &Wire, OLED_RESET);
 int menuPos = 1;
-
+float datos[3];
 void setup() {
   Wire.begin(); 
   oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  radio.begin();
   pinMode(2,INPUT);
   pinMode(3,INPUT);
   inicio();
   menu(menupos);
+  radio.openWritingPipe(direccion);
 }
 void loop() {
 button();
@@ -93,4 +103,21 @@ void button(){
     delay(100);
     while(!digitalRead(4));
   }
+}
+void envio(){
+   bool ok = radio.write(datos, sizeof(datos));
+   if(ok)
+  {
+     Serial.print("Datos enviados: "); 
+     Serial.print(datos[0]); 
+     Serial.print(" , "); 
+     Serial.print(datos[1]); 
+     Serial.print(" , "); 
+     Serial.println(datos[2]); 
+  }
+  else
+  {
+     Serial.println("no se ha podido enviar");
+  } 3ssa
+  delay(100);
 }
